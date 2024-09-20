ae33a766653a0e65c2fa22f9348e95186de987e6
Adrian-sosaa <a.22308051281317@cbtis128.edu.mx>
2024-09-20 09:37:27 -0600

    operadores funciones

---
+++ operadoresA.py
@@ -0,0 +1,49 @@
+print("Act 9 clases v2")
+print("Jan Sosa Mat: 22308051281317")
+#zona de clases
+class Operadores1317:
+    #zona de funciones
+    def suma1317(self,J,A):
+        s1317=J+A
+        print(f"La suma de {J} + {A} = {s1317}")
+    def resta1317(self,J,A):
+        s1317=J-A
+        print(f"La resta de {J} - {A} = {s1317}")
+    def multi1317(self,J,A):
+        s1317=J*A
+        print(f"La multiplicacion de {J} * {A} = {s1317}")
+    def divi1317(self,J,A):
+        s1317=J/A
+        print(f"La division de {J} / {A} = {s1317}")
+    def modu1317(self,J,A):
+        s1317=J%A
+        print(f"El modulo de {J} % {A} = {s1317}")
+    def expo1317(self,J,A):
+        s1317=J**A
+        print(f"La exponente de {J} ** {A} = {s1317}")
+    def coci1317(self,J,A):
+        s1317=J//A
+        print(f"La cociente de {J} // {A} = {s1317}")
+
+
+
+# creacion del creacion del objeto
+operasosa=Operadores1317()
+
+# zona de uso de objeto
+print(" funcion suma ")
+operasosa.suma1317(5,4)
+print(" funcion resta ")
+operasosa.resta1317(5,4)
+print(" funcion sumultiplicacion ")
+operasosa.multi1317(5,4)
+print(" funcion division ")
+operasosa.divi1317(5,4)
+print(" funcion modulo ")
+operasosa.modu1317(5,4)
+print(" funcion exponente ")
+operasosa.expo1317(5,4)
+print(" funcion cociente ")
+operasosa.coci1317(5,4)
+
+
